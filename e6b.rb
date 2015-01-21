f2f5e75f4ccb90bb3f636cd926842cfe8d5d08ee
Lyra-B <glykeriapeppa@gmail.com>
2015-01-21 12:21:44 +0000

    Refactored code

--- e6b.rb
+++ e6b.rb
@@ -1,34 +1,47 @@
-ba_12345 = {
-  :flight_number => 'BA-12345',
-  :airline => 'British Airways',
-  :from => "LHR",
-  :from_lat => 51.4700223,
-  :from_lng => -0.4542955,
-  :to => "JFK",
-  :to_lat => 40.6413111,
-  :to_lng => -73.77813909999999,
-  :flight_departure => Time.utc(2014, 11, 1, 14, 47),
-  :speed_kph => 790,
-  :bearing => 3,
-  :aircraft => "Boeing 747"
-}
+class Flight
+attr_accessor :flight_number, :airline, :from, :from_lat, :from_lng, :to,
+ :to_lat, :to_lng, :flight_departure, :speed_kph, :bearing, :aircraft
 
-# Find the estimated flight time- e.g. If an aircraft is flying at a speed of per hour, how long will it take to fly  miles?
-earth_radius_km = 6371
+  def estimated_flight_time
+    # Find the estimated flight time- e.g. If an aircraft is flying at a speed of per hour, how long will it take to fly  miles?
+    earth_radius_km = 6371
 
-# Convert the lat / lng from the from / to airports into radians
-from_lat_radians = ba_12345[:from_lat] * Math::PI / 180
-from_lng_radians = ba_12345[:from_lng] * Math::PI / 180
-to_lat_radians = ba_12345[:to_lat] * Math::PI / 180
-to_lng_radians = ba_12345[:to_lng] * Math::PI / 180
+    # Convert the lat / lng from the from / to airports into radians
+    from_lat_radians = from_lat * Math::PI / 180
+    from_lng_radians = from_lng * Math::PI / 180
+    to_lat_radians = to_lat * Math::PI / 180
+    to_lng_radians = to_lng * Math::PI / 180
 
-# Calculate the distance between the start and end airports
-cosines_product = Math.cos(to_lat_radians) * Math.cos(from_lat_radians) * Math.cos(from_lng_radians - to_lng_radians)
-sines_product = Math.sin(to_lat_radians) * Math.sin(from_lat_radians)
-distance = earth_radius_km * Math.acos(cosines_product + sines_product)
+    # Calculate the distance between the start and end airports
+    cosines_product = Math.cos(to_lat_radians) * Math.cos(from_lat_radians) * Math.cos(from_lng_radians - to_lng_radians)
+    sines_product = Math.sin(to_lat_radians) * Math.sin(from_lat_radians)
+    distance = earth_radius_km * Math.acos(cosines_product + sines_product)
 
-# Calculate the flight time
-speed = ba_12345[:speed_kph]
-hours = (distance / speed).round
-minutes = distance.remainder(speed).round
-puts "Estimated Flight Time for #{ba_12345[:flight_number]}: #{hours} hours #{minutes} minutes"
+    # Calculate the flight time
+    speed = speed_kph
+    hours = (distance / speed).round
+    minutes = distance.remainder(speed).round
+    return hours, minutes
+    #puts "Estimated Flight Time for #{ba_12345[:flight_number]}: #{hours} hours #{minutes} minutes"
+  end
+end
+
+flight = Flight.new
+  flight.flight_number = 'BA-12345',
+  flight.airline = 'British Airways',
+  flight.from = "LHR",
+  flight.from_lat = 51.4700223,
+  flight.from_lng = -0.4542955,
+  flight.to = "JFK",
+  flight.to_lat = 40.6413111,
+  flight.to_lng = -73.77813909999999,
+  flight.flight_departure = Time.utc(2014, 11, 1, 14, 47),
+  flight.speed_kph = 790,
+  flight.bearing = 3,
+  flight.aircraft = "Boeing 747"
+
+hours, minutes = flight.estimated_flight_time
+puts "Estimated Flight Time for #{flight.flight_number}: #{hours} hours #{minutes} minutes"
+
+
+
