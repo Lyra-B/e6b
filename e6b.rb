d1681e9fcec0f064495a74c3ccd07e92d858b86e
Lyra-B <glykeriapeppa@gmail.com>
2015-01-21 14:13:50 +0000

    Added LatLong Class

--- e6b.rb
+++ e6b.rb
@@ -1,16 +1,37 @@
+class LatLng
+  attr_accessor :lat, :lng
+
+  def initialize(lat, lng)
+    self.lat = lat
+    self.lng = lng
+  end
+
+  def to_radians
+    half_pi = Math::PI / 180
+    return lat * half_pi, lng * half_pi
+  end
+end
+
 class Airport
-  attr_accessor :code, :lat, :lng
+  attr_accessor :code, :lat_lng
 
   def distance_to(other_airport)
     earth_radius_km = 6371
-    from_lat_radians = @lat * Math::PI / 180
-    from_lng_radians = @lng * Math::PI / 180
-    to_lat_radians = other_airport.lat * Math::PI / 180
-    to_lng_radians = other_airport.lng * Math::PI / 180
+    from_lat_radians, from_lng_radians = lat_lng.to_radians
+    to_lat_radians, to_lng_radians = other_airport.lat_lng.to_radians
+    # from_lat_radians = @lat * Math::PI / 180
+    # from_lng_radians = @lng * Math::PI / 180
+    # to_lat_radians = other_airport.lat * Math::PI / 180
+    # to_lng_radians = other_airport.lng * Math::PI / 180
     cosines_product = Math.cos(to_lat_radians) * Math.cos(from_lat_radians) * Math.cos(from_lng_radians - to_lng_radians)
     sines_product = Math.sin(to_lat_radians) * Math.sin(from_lat_radians)
     distance = earth_radius_km * Math.acos(cosines_product + sines_product)
   end
+
+  # def lat_as_radians
+  #   lat * Math::PI / 180, lng * Math::PI / 180
+  # end
+
 end
 
 
@@ -28,13 +49,11 @@ end
 
 heathrow = Airport.new
 heathrow.code = "LHR"
-heathrow.lat = 51.4700223
-heathrow.lng = -0.4542955
+heathrow.lat_lng = LatLng.new(51.4700223,-0.4542955)
 
 jfk = Airport.new
 jfk.code = "JFK"
-jfk.lat = 40.6413111
-jfk.lng = -73.77813909999999
+jfk.lat_lng = LatLng.new(40.6413111, -73.77813909999999)
 
 flight = Flight.new
 flight.flight_number = 'BA-12345'
